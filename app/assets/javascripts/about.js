55f213c67aa82d1c667a60360d8b3be236be3022
Artur Hebda <artur@adtaily.com>
2012-01-31 12:50:28 +0100

    Refactoring of code handling the about section. Section moved to layout and js extracted to another file.

---
+++ app/assets/javascripts/about.js
@@ -0,0 +1,28 @@
+var about = {
+  isHidden: true,
+  selector: '.about',
+  duration: 200,
+
+  onResize: function() {
+    $(this.selector).css({ top: '100%' });
+  },
+
+  toggle: function() {
+    var callback = (this.isHidden) ? null : function() { $(".about").css({ top: '100%' }); };
+    $(this.selector).css({ top: this.computeTopOffset(this.isHidden) + 'px' }).animate({ top: this.computeTopOffset(! this.isHidden) + 'px' }, this.duration, callback);
+    this.isHidden = ! this.isHidden;
+  },
+
+  getHeight: function() {
+    return $(this.selector).height();
+  },
+
+  getWindowHeight: function() {
+    return $(window).height();
+  },
+
+  computeTopOffset: function(isHidden) {
+    var topOffset = (isHidden) ? this.getWindowHeight() : this.getWindowHeight() - this.getHeight();
+    return topOffset;
+  }
+};
